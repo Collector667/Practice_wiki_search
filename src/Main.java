797a14eed19f9ce8ee9994869cf9980a1da43d65
p9trusha <petrnevolin2005@gmail.com>
2024-11-14 15:51:26 +0300

    перемещение Main

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.awt.Desktop;
+
+class ConsoleInput {
+    Scanner in = new Scanner(System.in);
+
+    String getWikiRequest() {
+        System.out.print("Введите поисковой запрос: ");
+        String wikiRequest = this.in.nextLine();
+        return wikiRequest;
+    }
+
+    void close() {
+        this.in.close();
+    }
+}
+
+class OpenInBrowser {
+    Desktop
+    void wikipedia(int pageid) {
+    }
+}
+
+public class Main
+{
+    public static void main(String[] args) {
+        ConsoleInput cip = new ConsoleInput();
+        String wikiRequest = cip.getWikiRequest();
+        cip.close();
+    }
+}
