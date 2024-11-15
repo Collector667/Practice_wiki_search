e81ae78ec0e38821a5137d56e1ec1f6c9749e9da
p9trusha <petrnevolin2005@gmail.com>
2024-11-15 12:22:24 +0300

    console input

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
-public class Main {
+import java.util.Scanner;
+
+class ConsoleInput {
+    Scanner in = new Scanner(System.in);
+
+    String getWikiRequest() {
+        System.out.print("Введите поисковой запрос: ");
+        String wikiRequest = this.in.nextLine();
+        return wikiRequest;
+    }
+
+    void close() {
+        this.in.close();
+    }
+}
+
+public class Main
+{
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ConsoleInput cip = new ConsoleInput();
+        String wikiRequest = cip.getWikiRequest();
+        cip.close();
     }
 }
